b6559ec6f9d5d071e9313af92588a5eb7e4ae3a9
Vladimir Shishov <wladimir.shishov@gmail.com>
2020-04-19 12:07:37 +0300

    add project helloworld

---
+++ 01_helloworld/build/CMakeFiles/helloworld_cli.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/helloworld_cli.dir/main.cpp.o"
+  "helloworld_cli.pdb"
+  "helloworld_cli"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/helloworld_cli.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ 01_helloworld/build/CTestTestfile.cmake
@@ -0,0 +1,8 @@
+# CMake generated Testfile for 
+# Source directory: /home/bob/otus_cpp/01_helloworld
+# Build directory: /home/bob/otus_cpp/01_helloworld/build
+# 
+# This file includes the relevant testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.
+add_test(test_version "test_version")
+add_test(gtest_version "gtest_version")
