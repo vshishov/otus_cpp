cd69ab3ee0aa953ce65ef5f3fb0c818dfdc9a6da
Vladimir Shishov <wladimir.shishov@gmail.com>
2020-07-28 17:29:48 +0300

    delete for review

--- 02_ipfilter/src/common.h
+++
@@ -1,4 +0,0 @@
-#include <vector>
-#include <string>
-
-std::vector<std::string> split(const std::string &str, char d);
--- 02_ipfilter/src/version.cpp
+++
@@ -1,7 +0,0 @@
-#include "version.h"
-#include "versiondef.h"
-
-int version()
-{
-  return PROJECT_VERSION_PATCH;
-}
