78c9316422a7fd0f9b5ef845d6b6552760fac372
Vladimir Shishov <wladimir.shishov@gmail.com>
2020-07-28 17:31:53 +0300

    Review 3

---
+++ 02_ipfilter/src/main.cpp
@@ -0,0 +1,15 @@
+#include "ip_filter.h"
+
+#include <iostream>
+
+int main(int, char**)
+{
+  CIpPool<CIPv4> ipPool;
+  ipPool.read(std::cin); 
+  ipPool.sort_reverse();  
+  std::cout << ipPool << std::endl;
+  std::cout << ipPool.filter(1) << std::endl;
+  std::cout << ipPool.filter(46, 70) << std::endl;
+  std::cout << ipPool.filter_any(46) << std::endl;
+  return 0;
+}
